f650b039028034cd7bd3d6416ffbafbc047472a8
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2025-05-01 10:31:25 -0300

    matvecmul CUDA

--- NPB-RAYON/src/kernels/dot_product.cu
+++ NPB-RAYON/src/kernels/dot_product.cu
@@ -6,6 +6,7 @@
 #define CUDA_CHECK(call) \
     do { \
         cudaError_t err = call; \
+        printf("ERROR: %d", err);
         if (err != cudaSuccess) { \
             fprintf(stderr, "CUDA error in %s at line %d: %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
             exit(EXIT_FAILURE); \
