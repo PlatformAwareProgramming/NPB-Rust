372f0e9e0f45ba5f3e34db0d0dca245da263bdf2
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2025-05-01 20:11:27 -0300

    alloc_data function

--- NPB-RAYON/src/cg.rs
+++ NPB-RAYON/src/cg.rs
@@ -1268,24 +1268,13 @@ mod cg {
     #[kernelversion(acc_count=(AtLeast{val:1}), acc_backend=CUDA)]
     fn norm(x: &[f64], y: &[f64]) -> f64 {
 
-        let sum = (&x[0..(LASTCOL - FIRSTCOL + 1) as usize])
-            .into_iter()
-            .zip(&y[0..(LASTCOL - FIRSTCOL + 1) as usize])
-            .map(|(x, y)| {
-                let d = *x - *y;
-                d * d
-        })
-        .sum();
-
-        f64::sqrt(sum)
-
-      /*  let mut result: f64 = 0.0;
+        let mut result: f64 = 0.0;
     
         unsafe {
             launch_norm_gpu(x.as_ptr(), y.as_ptr(), &mut result as *mut f64, (LASTCOL - FIRSTCOL + 1) as i32);
         }
     
-        result*/    
+        result
     }
 
 
