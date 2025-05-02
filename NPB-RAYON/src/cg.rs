b3578967d121afe7ef358a8cc122fa767d543a34
decarvalhojunior-fh <heron@dc.ufc.br>
2025-05-02 08:54:26 -0300

    full gpu - testing

--- NPB-RAYON/src/cg.rs
+++ NPB-RAYON/src/cg.rs
@@ -1158,9 +1158,9 @@ mod cg {
     #[kernelversion(acc_count=(AtLeast{val:1}), acc_backend=CUDA)]
     fn move_a_to_device(colidx_h: &[i32],  rowstr_h: &[i32], a_h: &[f64], 
                         colidx_d: &mut [i32],  rowstr_d: &mut [i32], a_d: &mut [f64]) {
-        let nnz = a.len() as i32;
-        let num_rows = rowstr.len() as i32;
-        unsafe { move_a_to_device_gpu (colidx.as_ptr(), rowstr.as_ptr(), a.as_ptr(), nnz, num_rows) }
+        let nnz = a_h.len() as i32;
+        let num_rows = rowstr_h.len() as i32;
+        unsafe { move_a_to_device_gpu (colidx_h.as_ptr(), rowstr_h.as_ptr(), a_h.as_ptr(), nnz, num_rows) }
     }
 
     // y = a * x (sequential)
